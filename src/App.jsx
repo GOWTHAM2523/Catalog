ac0d6b63c956f2cbe05407fb8c637615697a1269
gowthamautorundms <111742898+GOWTHAM2523@users.noreply.github.com>
2025-12-06 19:58:53 +0530

    style updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Catalog from './components/Catalog'
 import './App.css'
+import Footer from './components/Footer.jsx'
 
 export default function App() {
   return (
     <div className="app">
       <Catalog />
+      <Footer />
     </div>
   )
 }
--- src/components/Catalog.jsx
+++ src/components/Catalog.jsx
@@ -3,7 +3,6 @@ import englishData from '../json/english.json'
 import tamilData from '../json/tamil.json'
 import tanglishData from '../json/tanglish.json'
 import ImageModal from './ImageModal'
-import Footer from './Footer'
 import '../styles/Catalog.css'
 import '../styles/Footer.css'
 
@@ -520,7 +519,7 @@ export default function Catalog() {
         />
       )}
 
-      <Footer />
+      
     </div>
   )
 }
